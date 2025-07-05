9f3d3f62ead13bf80ed9a13a0f5c69e2846d3a71
aaa308-aub <abdallahalawieh1@gmail.com>
2025-07-05 08:52:18 +0300

    Commit 4: handled floating point imprecision using the decimal library + some patches

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 class Calculator:
     def calculate(self, expression):  #calculate user-input expression
         tokens, stackSubExpressions = self.tokenize(expression)
@@ -8,21 +9,22 @@ class Calculator:
             for subExpression in arr:   #parse each sub-expression
                 self.parse(tokens, subExpression)
 
-        for token in tokens:  #return last remaining token
-            if token is not None:
-                #must handle edge-case where the input expression is 1 integer only
-                return int(token) if type(token) == str else token
+        for token in tokens:
+            if token is None: continue
+            result = float(token)
+            result = round(result, 25)
+            return int(result) if result.is_integer() else result
 
     def parse(self, tokens, subExpression):  #parse sub-expression (or entire expression)
 
         def parse_terms(index, operation):  #helper function to evaluate terms (+, -, *, /)
+            nonlocal start
             left, right = index-1, index+1
             while tokens[left] is None: left -= 1
             while tokens[right] is None: right += 1
-            x, y = float(tokens[left]), float(tokens[right])
-            # x and y must be cast to type float not int. If x (the left value) was a
-            # division term recently evaluated, it may not be a whole number.
-
+            x = Decimal(tokens[left]) if type(tokens[left]) == str else tokens[left]
+            y = Decimal(tokens[right]) if type(tokens[right]) == str else tokens[right]
+            # x and y must be Decimal objects to solve floating point imprecision.
             if operation == '*':
                 term = x*y
             elif operation == '+':
@@ -31,10 +33,7 @@ class Calculator:
                 term = x-y
             elif operation == '/':
                 term = x/y
-            term = round(term, 10)
-            if term.is_integer():
-                tokens[index] = int(term)
-            else: tokens[index] = term
+            tokens[index] = term
             tokens[left] = tokens[right] = None
 
         start, end = subExpression
@@ -42,26 +41,29 @@ class Calculator:
         if subExpression != (0, len(tokens)-1):
             tokens[start-1] = tokens[end+1] = None  #remove the parentheses using placeholder None
 
-        if tokens[start] in ['+','-']:  #handle leading + or - explicitly at the start
+        if tokens[start] in ['+','-']: #handle leading + or - explicitly at the start
             tokens[start+1] = tokens[start] + tokens[start+1]
             tokens[start] = None
             start += 1
 
         for index in range(start, end+1): #first pass: evaluate the terms (*, /), left to right
+            if type(tokens[index]) == str and tokens[index].isdigit():
+            #must be done early to handle edge-case of input expression being just one integer.
+                tokens[index] = Decimal(tokens[index])
             if tokens[index] in ['*','/']:
                 parse_terms(index, tokens[index])
-        
+
         for index in range(start, end+1): #second pass: evaluate the terms (+, -), left to right
             if tokens[index] in ['+','-']:
                 parse_terms(index, tokens[index])
 
-    def tokenize(self, expression):
+    def tokenize(self, expression):  #get array of tokens and sub-expressions start and end indices
         tokens = []
         stackOpenParentheses = []
         currentDepth = 0
         stackSubExpressions = [[]]
 
-        for index, char in enumerate(expression):
+        for index, char in enumerate(expression): #tokenize each character if valid; raise error if not.
             match char:
                 case '(':
                     if tokens != [] and (tokens[-1].isdigit() or tokens[-1] == ')'):
@@ -108,6 +110,8 @@ class Calculator:
                 case '*'|'/':
                     if tokens == [] or tokens[-1] == '(':
                         raise SyntaxError(f"Leading '*' or '-' Found -- raised at index {index}")
+                    if tokens[-1] in ['+','-','*','/']:
+                        raise SyntaxError(f"Adjacent Operators Found -- raised at index {index}")
                     if index == len(expression)-1:
                         raise SyntaxError(f"Trailing Operator Found -- raised at index {index}")
                     tokens.append(char)
@@ -124,17 +128,17 @@ class Calculator:
 
 def main():
     calc = Calculator()
-    print("Please enter your math expression. Currently,\nonly integers, parentheses, and basic operators (+,-,*,/)\nare allowed. If you wish to exit the calculator, type 'Q'")
+    print("Please enter your math expression. Currently,\nonly integers, parentheses, and basic operators (+,-,*,/)\nare allowed. If you wish to quit the calculator, type 'Q'")
     while True:
         expression = input("> ")
         if expression == 'Q': break
         try:
             result = calc.calculate(expression)
-            print(result)
-            print(type(result), end='\n\n')
+            print(result, end='\n\n')
         except SyntaxError as s:
             print(f"SYNTAX-ERROR: {s}", end='\n\n')
         except ZeroDivisionError:
             print("ZERO DIVISION ERROR: Cannot divide by zero!", end='\n\n')
+
 if __name__ == "__main__":
     main()
